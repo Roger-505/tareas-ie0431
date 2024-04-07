bbe46e43be4adca0df00561b2d15b11704cc526c
rogerdanielpiovetgarcia@gmail.com <rogerdanielpiovetgarcia@gmail.com>
2024-04-06 20:58:02 -0600

    feat: Se agregaron textos en LaTeX de los gráficos, y se implementó la parte B de la segunda parte de la tarea

--- t1/src/tarea1.m
+++ t1/src/tarea1.m
@@ -58,9 +58,7 @@ uA = urA + udA;
 
 % Grafica de respuesta del sistema
 figure (1) ;
-title ('Respuesta del sistema como Servocontrol y como Regulador');
-xlabel ('Tiempo (s)');
-ylabel ('Respuesta del sistema');
+x1=xlabel ('$t$ [s]');
 hold on;
 plot (t , yA, ...
     'LineWidth', 2.5, ...
@@ -70,16 +68,18 @@ plot (t , d, '--', ...
     'Color', 'b');
 plot (t , r, '--', ...
     'LineWidth', 2.5, ...
-    'Color', 'r');
+    'Color', 'k');
 grid on;
-legend('y(t)', 'd(t)', 'r(t)')
+leg1=legend('$y(t)$', '$d(t)$', '$r(t)$');
+set(x1,'Interpreter','latex');
+set(x1,'FontSize',12);
+set(leg1,'Interpreter','latex');
+set(leg1,'FontSize',12);
 hold off;
 
 % Grafica de respuesta del controlador
 figure (2) ;
-title ('Respuesta del Controlador como Servomecanismo y como Regulador');
-xlabel ('Tiempo (s)');
-ylabel ('Respuesta del controlador');
+x2=xlabel ('$t$ [s]');
 hold on;
 plot (t , uA, ...
     'LineWidth', 2.5, ...
@@ -89,11 +89,88 @@ plot (t , d, '--', ...
     'Color', 'b');
 plot (t , r, '--', ...
     'LineWidth', 2.5, ...
-    'Color', 'r');
+    'Color', 'k');
 grid on;
-legend('u(t)', 'd(t)', 'r(t)')
+leg2=legend('$u(t)$', '$d(t)$', '$r(t)$');
+set(x2,'Interpreter','latex');
+set(x2,'FontSize',12);
+set(leg2,'Interpreter','latex');
+set(leg2,'FontSize',12);
 hold off;
 
 %% PARTE B
-G
+% Declaracion de parametros
+Kp = 2;
+Ti = 2;
 
+% Controlador integrante
+C = Kp*(1+ 1/(Ti * s));
+
+% Sistema como servocontrol y regulador
+% Respuesta del sistema
+MydB = minreal(P/(1+ C * P));
+MyrB = minreal(MydB * C);
+
+% Respuesta del controlador
+MurB = minreal(C/(1+ C * P));
+MudB = minreal(MydB * -P);
+
+% Referencia unitaria a partir de t = 1,
+% perturbacion unitaria a partir de t = 6
+r = 0;
+r ( t >= 1) = 1;
+d = 0; 
+d ( t >= 6) = 1;
+
+% Simulacion de servocontrol y regulador
+% Respuesta del sistema
+yrB = lsim(MyrB, r ,t);
+ydB = lsim(MydB ,d ,t );
+yB = yrB + ydB;
+
+% Respuesta del controlador
+urB = lsim(MurB, r ,t);
+udB = lsim(MudB ,d ,t );
+uB = urA + udB;
+
+% Grafica de respuesta del sistema
+figure (3) ;
+x3=xlabel ('$t$ [s]');
+hold on;
+plot (t , yB, ...
+    'LineWidth', 2.5, ...
+    'Color', 'r');
+plot (t , d, '--', ...
+    'LineWidth', 2.5, ...
+    'Color', 'b');
+plot (t , r, '--', ...
+    'LineWidth', 2.5, ...
+    'Color', 'k');
+grid on;
+leg3=legend('$y(t)$', '$d(t)$', '$r(t)$');
+set(x3,'Interpreter','latex');
+set(x3,'FontSize',12);
+set(leg3,'Interpreter','latex');
+set(leg3,'FontSize',12);
+hold off;
+
+% Grafica de respuesta del controlador
+figure (4) ;
+x4=xlabel ('$t$ [s]');
+hold on;
+plot (t , uB, ...
+    'LineWidth', 2.5, ...
+    'Color', 'r');
+plot (t , d, '--', ...
+    'LineWidth', 2.5, ...
+    'Color', 'b');
+plot (t , r, '--', ...
+    'LineWidth', 2.5, ...
+    'Color', 'k');
+grid on;
+leg4=legend('$u(t)$', '$d(t)$', '$r(t)$');
+set(x4,'Interpreter','latex');
+set(x4,'FontSize',12);
+set(leg4,'Interpreter','latex');
+set(leg4,'FontSize',12);
+hold off;
